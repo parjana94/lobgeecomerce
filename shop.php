db313d22d791d49a79d55130bdd77cd8b229c3b7
parjana94 <b.parjana@gmail.com>
2023-08-13 15:54:21 +0400

    ss

--- shop.php
+++ shop.php
@@ -83,10 +83,10 @@
                    
                    <ul class="nav navbar-nav left"><!-- nav navbar-nav left Begin -->
                        
-                       <li class="active">
+                       <li >
                            <a href="index.php">Home</a>
                        </li>
-                       <li>
+                       <li class="active" >
                            <a href="shop.php">Shop</a>
                        </li>
                        <li>
@@ -151,127 +151,33 @@
            
        </div><!-- container Finish -->
        
-   </div><!-- navbar navbar-default Finish -->
-   
-   <div class="container" id="slider"><!-- container Begin -->
-       
-       <div class="col-md-12"><!-- col-md-12 Begin -->
-           
-           <div class="carousel slide" id="myCarousel" data-ride="carousel"><!-- carousel slide Begin -->
-               
-               <ol class="carousel-indicators"><!-- carousel-indicators Begin -->
-                   
-                   <li class="active" data-target="#myCarousel" data-slide-to="0"></li>
-                   <li data-target="#myCarousel" data-slide-to="1"></li>
-                   <li data-target="#myCarousel" data-slide-to="2"></li>
-                   <li data-target="#myCarousel" data-slide-to="3"></li>
-                   
-               </ol><!-- carousel-indicators Finish -->
-               
-               <div class="carousel-inner"><!-- carousel-inner Begin -->
-                   
-                   <div class="item active">
-                       
-                       <img src="admin_area/slides_images/slide-1.jpg" alt="Slider Image 1">
-                       
-                   </div>
-                   
-                   <div class="item">
-                       
-                       <img src="admin_area/slides_images/slide-2.jpg" alt="Slider Image 2">
-                       
-                   </div>
-                   
-                   <div class="item">
-                       
-                       <img src="admin_area/slides_images/slide-3.jpg" alt="Slider Image 3">
-                       
-                   </div>
-                   
-                   <div class="item">
-                       
-                       <img src="admin_area/slides_images/slide-4.jpg" alt="Slider Image 4">
-                       
-                   </div>
-                   
-               </div><!-- carousel-inner Finish -->
-               
-               <a href="#myCarousel" class="left carousel-control" data-slide="prev"><!-- left carousel-control Begin -->
-                   
-                   <span class="glyphicon glyphicon-chevron-left"></span>
-                   <span class="sr-only">Previous</span>
-                   
-               </a><!-- left carousel-control Finish -->
-               
-               <a href="#myCarousel" class="right carousel-control" data-slide="next"><!-- left carousel-control Begin -->
-                   
-                   <span class="glyphicon glyphicon-chevron-right"></span>
-                   <span class="sr-only">Next</span>
-                   
-               </a><!-- left carousel-control Finish -->
-               
-           </div><!-- carousel slide Finish -->
-           
-       </div><!-- col-md-12 Finish -->
-       
-   </div><!-- container Finish -->
-   
-   <div id="adventages"><!-- adventages Begin -->
-        <div class="container"><!-- conatiner Begin -->
-            <div class="same-height-row"><!--same-height-row Begin -->
-                <div class="col-sm-4"><!--col-sm-4 Begin -->
-                    <div class="box same-height"><!--box same-height Begin -->
-                        <dic class="icon"><!--icon Begin -->
-                            <i class="fa fa-heart"></i>
-                        </dic><!--icon Finish -->
-                        <h3><a href="#">Best Offer</a></h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                        </p>
-                    </div><!--box same-height Finish -->
-                </div><!--col-sm-4 Finish -->
-                <div class="col-sm-4"><!--col-sm-4 Begin -->
-                    <div class="box same-height"><!--box same-height Begin -->
-                        <dic class="icon"><!--icon Begin -->
-                            <i class="fa fa-tag"></i>
-                        </dic><!--icon Finish -->
-                        <h3><a href="#">Best Prices</a></h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.?</p>
-                    </div><!--box same-height Finish -->
-                </div><!--col-sm-4 Finish -->
-                
-                <div class="col-sm-4"><!--col-sm-4 Begin -->
-                    <div class="box same-height"><!--box same-height Begin -->
-                        <dic class="icon"><!--icon Begin -->
-                            <i class="fa fa-thumbs-up"></i>
-                        </dic><!--icon Finish -->
-                        <h3><a href="#">100% Original</a></h3>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. .</p>
-                    </div><!--box same-height Finish -->
-                </div><!--col-sm-4 Finish -->
-            </div><!--same-height-row Finish -->
-        </div><!-- container Finish -->
-   </div><!-- adventages Finish -->
-    
-    <div id="hot"><!-- #hot Begin -->
+   </div><!-- navbar navbar-default Finish --> 
 
-        <div class="box"><!-- box Begin -->
-            <div class="container"><!-- container Begin -->
-                <div class="col-md-12"><!-- col-md-12r Begin -->
-                    <h2>
-                        Our Latest Products
-                    </h2>
-                </div><!-- col-md-12 Finish -->
-            </div><!-- container Finish -->
-        </div><!-- box Finish -->
-
-    </div><!-- #hot Finish -->
-
-    <div id="content" class="container" ><!-- container Begin -->
-            <div class="row"><!-- row Begin -->
-
-                <div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Begin -->
-
-                    <div class="product"><!-- product Begin -->
+   <div id="content"><!-- #content Begin -->
+    <div class="container"><!-- container Begin -->
+     <div class="col-md-12"><!-- col-md-12 Begin -->
+         <ul class="breadcrumb"><!-- breadcrumb Begin -->
+          <li>
+           <a href="index.php">Home</a>
+          </li>
+          <li>
+           Shop
+          </li>
+         </ul><!-- breadcrumb Finish -->
+     </div><!-- col-md-12 Finish -->
+      <div class="col-md-3"><!--col-md-3 Begin -->
+           <?php 
+    include("includes/sidebar.php");
+    ?>
+      </div><!--col-md-3 Finish -->
+      <div class="col-md-9"><!--col-md-9 Begin -->
+       <div class="box"><!--box Begin -->
+        <h1>Shop</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga beatae aliquam enim, perspiciatis neque odit unde. Eius repellat ratione voluptate maiores, voluptatum deleniti labore beatae pariatur amet, dolores cum sequi.</p>
+       </div><!--box Finish -->
+       <div class="row"><!--row Begin -->
+        <div class="col-md-4 col-sm-6 center-responsive"><!--col-md-4 col-sm-6 center-responsive Begin -->
+                               <div class="product"><!-- product Begin -->
 
                         <a href="details.php">
                             <img class="img-responsive"  src="admin_area/product_images/2.jpeg" alt="">
@@ -297,43 +203,9 @@
                         </div><!-- text Finish -->
 
                     </div><!-- product Finish -->
-
-
-                </div><!-- col-sm-4 col-sm-6 single- Finish -->                
-                <div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Begin -->
-
-                    <div class="product"><!-- product Begin -->
-
-                        <a href="details.php">
-                            <img class="img-responsive"  src="admin_area/product_images/2.jpeg" alt="">
-                        </a>
-
-                        <div class="text"><!-- text Begin -->
-                            <h3>
-                                <a href="details.php">
-                                    M-Dev Women T-shirt
-                                </a>
-                            </h3>
-                            <p class="price">
-                                $30
-                            </p>
-                            <p class="button">
-                                <a href="details.php" class="btn btn-default">View Details</a>
-                                <a href="details.php" class="btn btn-primary">
-                                    <i class="fa fa-shopping-cart">
-                                        Add To Cart
-                                    </i>
-                                </a>
-                            </p>
-                        </div><!-- text Finish -->
-
-                    </div><!-- product Finish -->
-
-
-                </div><!-- col-sm-4 col-sm-6 single- Finish -->                
-                <div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Begin -->
-
-                    <div class="product"><!-- product Begin -->
+        </div><!--col-md-4 col-sm-6 center-responsive Finish -->
+        <div class="col-md-4 col-sm-6 center-responsive"><!--col-md-4 col-sm-6 center-responsive Begin -->
+                               <div class="product"><!-- product Begin -->
 
                         <a href="details.php">
                             <img class="img-responsive"  src="admin_area/product_images/2.jpeg" alt="">
@@ -359,43 +231,9 @@
                         </div><!-- text Finish -->
 
                     </div><!-- product Finish -->
-
-
-                </div><!-- col-sm-4 col-sm-6 single- Finish -->
-                <div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Begin -->
-
-                    <div class="product"><!-- product Begin -->
-
-                        <a href="details.php">
-                            <img class="img-responsive"  src="admin_area/product_images/2.jpeg" alt="">
-                        </a>
-
-                        <div class="text"><!-- text Begin -->
-                            <h3>
-                                <a href="details.php">
-                                    M-Dev Women T-shirt
-                                </a>
-                            </h3>
-                            <p class="price">
-                                $30
-                            </p>
-                            <p class="button">
-                                <a href="details.php" class="btn btn-default">View Details</a>
-                                <a href="details.php" class="btn btn-primary">
-                                    <i class="fa fa-shopping-cart">
-                                        Add To Cart
-                                    </i>
-                                </a>
-                            </p>
-                        </div><!-- text Finish -->
-
-                    </div><!-- product Finish -->
-
-
-                </div><!-- col-sm-4 col-sm-6 single Finish -->               
-                <div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Begin -->
-
-                    <div class="product"><!-- product Begin -->
+        </div><!--col-md-4 col-sm-6 center-responsive Finish -->
+        <div class="col-md-4 col-sm-6 center-responsive"><!--col-md-4 col-sm-6 center-responsive Begin -->
+                               <div class="product"><!-- product Begin -->
 
                         <a href="details.php">
                             <img class="img-responsive"  src="admin_area/product_images/2.jpeg" alt="">
@@ -421,43 +259,9 @@
                         </div><!-- text Finish -->
 
                     </div><!-- product Finish -->
-
-
-                </div><!-- col-sm-4 col-sm-6 single- Finish -->                
-                <div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Begin -->
-
-                    <div class="product"><!-- product Begin -->
-
-                        <a href="details.php">
-                            <img class="img-responsive"  src="admin_area/product_images/2.jpeg" alt="">
-                        </a>
-
-                        <div class="text"><!-- text Begin -->
-                            <h3>
-                                <a href="details.php">
-                                    M-Dev Women T-shirt
-                                </a>
-                            </h3>
-                            <p class="price">
-                                $30
-                            </p>
-                            <p class="button">
-                                <a href="details.php" class="btn btn-default">View Details</a>
-                                <a href="details.php" class="btn btn-primary">
-                                    <i class="fa fa-shopping-cart">
-                                        Add To Cart
-                                    </i>
-                                </a>
-                            </p>
-                        </div><!-- text Finish -->
-
-                    </div><!-- product Finish -->
-
-
-                </div><!-- col-sm-4 col-sm-6 single- Finish -->                
-                <div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Begin -->
-
-                    <div class="product"><!-- product Begin -->
+        </div><!--col-md-4 col-sm-6 center-responsive Finish -->
+        <div class="col-md-4 col-sm-6 center-responsive"><!--col-md-4 col-sm-6 center-responsive Begin -->
+                               <div class="product"><!-- product Begin -->
 
                         <a href="details.php">
                             <img class="img-responsive"  src="admin_area/product_images/2.jpeg" alt="">
@@ -483,12 +287,9 @@
                         </div><!-- text Finish -->
 
                     </div><!-- product Finish -->
-
-
-                </div><!-- col-sm-4 col-sm-6 single- Finish -->
-                <div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Begin -->
-
-                    <div class="product"><!-- product Begin -->
+        </div><!--col-md-4 col-sm-6 center-responsive Finish -->
+        <div class="col-md-4 col-sm-6 center-responsive"><!--col-md-4 col-sm-6 center-responsive Begin -->
+                               <div class="product"><!-- product Begin -->
 
                         <a href="details.php">
                             <img class="img-responsive"  src="admin_area/product_images/2.jpeg" alt="">
@@ -497,7 +298,7 @@
                         <div class="text"><!-- text Begin -->
                             <h3>
                                 <a href="details.php">
-                                    M-Dev Women T-shirt
+                                    M-Dev Men T-shirt
                                 </a>
                             </h3>
                             <p class="price">
@@ -514,14 +315,50 @@
                         </div><!-- text Finish -->
 
                     </div><!-- product Finish -->
+        </div><!--col-md-4 col-sm-6 center-responsive Finish -->
+        <div class="col-md-4 col-sm-6 center-responsive"><!--col-md-4 col-sm-6 center-responsive Begin -->
+                               <div class="product"><!-- product Begin -->
 
+                        <a href="details.php">
+                            <img class="img-responsive"  src="admin_area/product_images/2.jpeg" alt="">
+                        </a>
 
-                </div><!-- col-sm-4 col-sm-6 single Finish -->
-            </div><!-- row Finish -->
+                        <div class="text"><!-- text Begin -->
+                            <h3>
+                                <a href="details.php">
+                                    M-Dev Men T-shirt
+                                </a>
+                            </h3>
+                            <p class="price">
+                                $30
+                            </p>
+                            <p class="button">
+                                <a href="details.php" class="btn btn-default">View Details</a>
+                                <a href="details.php" class="btn btn-primary">
+                                    <i class="fa fa-shopping-cart">
+                                        Add To Cart
+                                    </i>
+                                </a>
+                            </p>
+                        </div><!-- text Finish -->
 
-        </div><!-- container Finish -->
-
-
+                    </div><!-- product Finish -->
+        </div><!--col-md-4 col-sm-6 center-responsive Finish -->
+       </div><!--row Finish -->
+       <center>
+        <ul class="pagination">
+         <li><a href="#">First Page</a></li>
+         <li><a href="#">1</a></li>
+         <li><a href="#">2</a></li>
+         <li><a href="#">3</a></li>
+         <li><a href="#">4</a></li>
+         <li><a href="3">5</a></li>
+         <li><a href="#">Last Page</a></li>
+        </ul>
+       </center>
+      </div><!--col-md-9 Finish -->
+    </div><!-- container Finish -->
+   </div><!-- #content Finish -->
     
     <?php 
     include("includes/footer.php");
